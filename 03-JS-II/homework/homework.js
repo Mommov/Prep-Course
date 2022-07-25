7e4bebe250f51bc81588a1c968d7b532e1f6907c
Lucas Picarelli <picarbu@gmail.com>
2022-07-25 15:48:00 -0300

    hay 3 mal

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -98,7 +98,16 @@ function estaEnRango(numero) {
   return false;
 }
 
-
+function esEntero(numero) {
+  // Devuelve "true" si "numero" es un entero (int/integer)
+  // Ejemplo: 0.8 -> false
+  // Ejemplo: 1 -> true
+  // Ejemplo: -10 -> true
+  // De lo contrario, devuelve "false"
+  // Pista: Puedes resolver esto usando `Math.floor`
+  // Tu código:
+  return numero % 1 === 0;
+}
 
 function fizzBuzz(numero) {
   // Si "numero" es divisible entre 3, devuelve "fizz"
@@ -162,23 +171,27 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-let arraytablaDelsix = []
-  for (let i = 0; i < 11; i++) {
-    arraytablaDelsix.push (6 * i);
+let arraytablaDelSeis = []
+  for (let i = 0; i < 11 ; i++) {
+    arraytablaDelSeis.push(6 * i);
   }
-  return arraytablaDelsix;  
+  return arraytablaDelSeis;
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
-}
+  if (numero > 99 && numero < 1000) {
+    return true;
+  } 
+  return false;
+  }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  
 }
 
 
