9296751bb0cd902bd2dbc7f6da0efb3d0f5c164c
Lucas Picarelli <picarbu@gmail.com>
2022-07-25 13:59:11 -0300

    algo falla

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -137,12 +137,24 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero < 2 )  return false; 
+  if (numero === 2) return true;
+  for (var i = 2; i < numero; i++) {
+    if (numero % i == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if (valor === true) {
+    return 'Soy verdadero';
+  }
+  return 'Soy falso'
 
 }
 
@@ -150,7 +162,11 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+let arraytablaDelsix = []
+  for (let i = 0; i < 11; i++) {
+    arraytablaDelsix.push (6 * i);
+  }
+  return arraytablaDelsix;  
 }
 
 function tieneTresDigitos(numero){
