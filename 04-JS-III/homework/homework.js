5173d3a2f16590f935f88f8b07a200a14cbcd124
Lucas Picarelli <picarbu@gmail.com>
2022-07-26 19:46:14 -0300

    aprovado

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -246,8 +246,19 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
-  
+  var caja2 = [];
+  var suma = numero;
+  for (var i = 0; i < 10; i++){
+   if (i === 5){
+      continue;      
+    } 
+    suma = suma + 2;
+    caja2.push(suma);
+    }
+    return caja2;   
 }
+
+
 
 
 // No modificar nada debajo de esta línea
